0d17a76ace386d2e466300db85c2b1b6def49621
Hiromu Ochiai <otiai10@gmail.com>
2019-04-24 00:51:12 +0900

    PoC

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
+const lookpath = require('lookpath');
+const { execSync } = require('child_process');
+const path = require('path');
 
 const __main__ = () => {
   const win = new BrowserWindow({
@@ -7,6 +11,28 @@ const __main__ = () => {
     x: 40, y: 40,
   });
   win.loadURL(`file://${__dirname}/dist/index.html`)
+
+  ipcMain.on('file', async (ev, source) => {
+    const destdir = '/Users/otiai10/Desktop'; // FIXME
+    if (source.type === 'audio/mp3') {
+      console.log(1000);
+      return fs.copyFileSync(source.path, path.join(destdir, path.basename(source.path)));
+    }
+    if (!fs.existsSync(source.path)) {
+      console.log(2000);
+      return ev.sender.send('failed');
+    }
+    const ffmpeg = await lookpath('ffmpeg');
+    if (!ffmpeg) {
+      console.log(3000);
+      return ev.sender.send('failed');
+    }
+    source.path = source.path.replace(/(\s+)/g, '\\$1');
+    const destpath = path.join(destdir, path.basename(source.path).replace(path.extname(source.path), '.mp3'));
+    console.log(source.path, destpath);
+    console.log(ffmpeg);
+    execSync(`${ffmpeg} -i ${source.path} ${destpath}`);
+  });
 };
 
 app.on('ready', __main__);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var window: {
+  ipcRenderer: {
+    send: (name: string, args: any) => void;
+    on: (name: string, listener: (event: Event, args: any) => void) => void;
+  }
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sprouter';
+export class AppComponent implements OnInit {
+
+  isDraggedOver = false;
+  isProcessing = false;
+
+  ngOnInit() {
+    window.ipcRenderer.on('success', (ev, args) => {
+      console.log('SUCCESS:', args);
+      this.isProcessing = false;
+    });
+  }
+
+  onDrop(ev: DragEvent) {
+    ev.stopPropagation();
+    ev.preventDefault();
+    const file = ev.dataTransfer.files[0];
+    if (!file) {
+      return;
+    }
+    const { name, path, size, type } = file as any;
+    window.ipcRenderer.send('file', { name, path, size, type });
+    this.isProcessing = true;
+  }
+
+  onDragOver(ev) {
+    ev.stopPropagation();
+    ev.preventDefault();
+    this.isDraggedOver = true;
+  }
+
+  onDragLeave(ev) {
+    this.isDraggedOver = false;
+  }
+
 }
